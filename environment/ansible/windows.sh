e6730fad55720299f74836f24a114478a4e04bc1
Jannik Portz <hello@jannikportz.de>
2016-04-09 23:28:32 +0200

    updated remaining paths in windows.sh

--- environment/ansible/windows.sh
+++ environment/ansible/windows.sh
@@ -25,7 +25,7 @@ sudo apt-get update
 sudo apt-get install -y ansible
 
 # Setup Ansible for Local Use and Run
-cp /vagrant/ansible/inventories/dev /etc/ansible/hosts -f
+cp /vagrant/environment/ansible/inventories/dev /etc/ansible/hosts -f
 chmod 666 /etc/ansible/hosts
-cat /vagrant/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
+cat /vagrant/environment/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
 sudo ansible-playbook /vagrant/environment/ansible/playbook.yml -e hostname=$1 --connection=local
