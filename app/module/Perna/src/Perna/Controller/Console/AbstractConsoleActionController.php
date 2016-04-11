ebd945f962287ae7dd221c269a5376bc154d58e8
Jannik Portz <hello@jannikportz.de>
2016-04-11 10:57:33 +0200

    added class AbstractConsoleActionController

---
+++ app/module/Perna/src/Perna/Controller/Console/AbstractConsoleActionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Perna\Controller\Console;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\MvcEvent;
+use Zend\Console\Request as ConsoleRequest;
+use Exception;
+
+/**
+ * Abstraction for a Controller for Console Actions
+ *
+ * @author      Jannik Portz
+ * @package     Perna\Controller\Console
+ */
+abstract class AbstractConsoleActionController extends AbstractActionController {
+
+	/**
+	 * Ensure that controller is dispatched from console
+	 * @inheritdoc
+	 * @throws    Exception If controller has not been dispatched from console
+	 */
+	public function onDispatch( MvcEvent $e ) : mixed {
+		$request = $e->getRequest();
+		if ( !$request instanceof ConsoleRequest )
+			throw new Exception("You may only run this action from a command line");
+
+		return parent::onDispatch( $e );
+	}
+
+}
