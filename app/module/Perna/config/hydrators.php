1dfa68630a8195baf5a147eb308dfd39ab48d58b
Jannik Portz <hello@jannikportz.de>
2016-04-23 16:43:04 +0200

    implemented Userhydrator

--- app/module/Perna/config/hydrators.php
+++ app/module/Perna/config/hydrators.php
@@ -1,9 +1,11 @@
 <?php
 
 use Perna\Hydrator\CityDumpHydrator;
+use Perna\Hydrator\UserHydrator;
 
 return [
 	'invokables' => [
-		CityDumpHydrator::class => CityDumpHydrator::class
+		CityDumpHydrator::class => CityDumpHydrator::class,
+		UserHydrator::class => UserHydrator::class
 	]
 ];
