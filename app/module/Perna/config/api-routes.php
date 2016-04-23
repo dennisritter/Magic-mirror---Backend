1f8c6b2cb77566d3c22c17c7d5f5f88654097723
Jannik Portz <hello@jannikportz.de>
2016-04-23 18:54:05 +0200

    added route for Registration

--- app/module/Perna/config/api-routes.php
+++ app/module/Perna/config/api-routes.php
@@ -1,3 +1,16 @@
 <?php
 
-return [];
+use Perna\Controller\UserController;
+use Zend\Mvc\Router\Http\Literal;
+
+return [
+	'register' => [
+		'type' => Literal::class,
+		'options' => [
+			'route' => '/register',
+			'defaults' => [
+				'controller' => UserController::class
+			]
+		]
+	]
+];
