5552ebc7787151535240da51a4b5b4fe7e520984
Jannik Portz <hello@jannikportz.de>
2016-04-23 18:53:21 +0200

    removed name pattern in InputFactory

--- app/module/Perna/src/Perna/InputFilter/InputFactory.php
+++ app/module/Perna/src/Perna/InputFilter/InputFactory.php
@@ -10,14 +10,12 @@ use Zend\Validator\StringLength;
 
 class InputFactory {
 
-	const NAME_PATTERN = '/^([ \u00c0-\u01ffa-zA-Z\'\-])+$/';
-
 	public static function createNameInput( string $name ) {
 		$input = new Input( $name );
 		$input->getValidatorChain()->attach( new StringLength([
 			'min' => 2,
 			'max' => 100
-		]) )->attach( new Regex( self::NAME_PATTERN ) );
+		]) );
 		$input->getFilterChain()->attach( new StringTrim() );
 
 		return $input;
