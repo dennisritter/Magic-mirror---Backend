4ecb58fe1477d2688ce8425218ff7b113537257c
Jannik Portz <hello@jannikportz.de>
2016-04-24 16:45:32 +0200

    added UserTokenHydrator

---
+++ app/module/Perna/src/Perna/Hydrator/UserTokenHydrator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Perna\Hydrator;
+
+use Perna\Document\UserToken;
+use Zend\Hydrator\HydratorInterface;
+
+/**
+ * Hydrator for all kinds of UserToken
+ *
+ * @author      Jannik Portz
+ * @package     Perna\Hydrator
+ */
+class UserTokenHydrator implements HydratorInterface {
+
+	/** @inheritdoc */
+	public function extract ( $object ) : array {
+		/** @var UserToken $object */
+		return [
+			'token' => $object->getToken(),
+			'expires' => $object->getExpires(),
+			'expirationDate' => $object->getExpirationDate()
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 *
+	 * A UserToken cannot be hydrated
+	 */
+	public function hydrate ( array $data, $object ) : object {
+		return $object;
+	}
+}
