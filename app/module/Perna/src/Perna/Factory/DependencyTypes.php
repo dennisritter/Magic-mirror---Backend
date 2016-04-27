45433066c90c646228efbe37d65e883cc6f4ef11
Jannik Portz <hello@jannikportz.de>
2016-04-27 16:04:05 +0200

    added intitial implementation of Factory

---
+++ app/module/Perna/src/Perna/Factory/DependencyTypes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Perna\Factory;
+
+/**
+ * Contains constants with dependency types
+ *
+ * @author      Jannik Portz
+ * @package     Perna\Factory
+ */
+class DependencyTypes {
+
+	const SERVICE = 'service';
+	const HYDRATOR = 'hydrator';
+	const INPUT_FILTER = 'input-filter';
+
+}
