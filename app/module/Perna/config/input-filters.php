492852fa2640000a7bd23c0fa6ea1a94c5272c65
Jannik Portz <hello@jannikportz.de>
2016-05-11 18:27:41 +0200

    added EventQuickAddInputFilter

--- app/module/Perna/config/input-filters.php
+++ app/module/Perna/config/input-filters.php
@@ -1,6 +1,7 @@
 <?php
 
 use Perna\InputFilter\CityDumpInputFilter;
+use Perna\InputFilter\EventQuickAddInputFilter;
 use Perna\InputFilter\LoginCredentialsInputFilter;
 use Perna\InputFilter\RefreshInputFilter;
 use Perna\InputFilter\UserInputFilter;
@@ -12,6 +13,7 @@ return [
 		LoginCredentialsInputFilter::class => LoginCredentialsInputFilter::class,
 		UserInputFilter::class => UserInputFilter::class,
 		UserPutInputFilter::class => UserPutInputFilter::class,
-		RefreshInputFilter::class => RefreshInputFilter::class
+		RefreshInputFilter::class => RefreshInputFilter::class,
+		EventQuickAddInputFilter::class => EventQuickAddInputFilter::class
 	],
 ];
