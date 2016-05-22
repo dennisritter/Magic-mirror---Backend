b61499b5aad1d6a81e0c5f6b9baf034c2b70cf16
Jannik Portz <hello@jannikportz.de>
2016-05-22 13:53:46 +0200

    code style in Bootstrap fie

--- app/module/Perna/test/Bootstrap.php
+++ app/module/Perna/test/Bootstrap.php
@@ -87,8 +87,7 @@ class Bootstrap {
 		));
 	}
 
-	protected static function findParentPath($path)
-	{
+	protected static function findParentPath($path) {
 		$dir = __DIR__;
 		$previousDir = '.';
 		while (!is_dir($dir . '/' . $path)) {
