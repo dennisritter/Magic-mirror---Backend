35e91963fb5f36fd4a69c1e0ee3648947d28b5be
Jannik Portz <hello@jannikportz.de>
2016-05-25 15:22:46 +0200

    throw 422 exception when geo coordinates are not present in query

--- app/module/Perna/src/Perna/Controller/Weather/WeatherLocationNearbyController.php
+++ app/module/Perna/src/Perna/Controller/Weather/WeatherLocationNearbyController.php
@@ -6,6 +6,7 @@ use Perna\Controller\AbstractAuthenticatedApiController;
 use Perna\Hydrator\CityHydrator;
 use Perna\Service\AuthenticationService;
 use Perna\Service\WeatherLocationService;
+use ZfrRest\Http\Exception\Client\UnprocessableEntityException;
 
 class WeatherLocationNearbyController extends AbstractAuthenticatedApiController {
 
@@ -20,11 +21,15 @@ class WeatherLocationNearbyController extends AbstractAuthenticatedApiController
 	}
 
 	public function get () {
-		//$this->assertAccessToken();
+		$this->assertAccessToken();
 		$params = $this->params();
-		$lat = floatval( $params->fromQuery('latitude') );
-		$lng = floatval( $params->fromQuery('longitude') );
+
+		$lat = floatval( $params->fromQuery('latitude', null) );
+		$lng = floatval( $params->fromQuery('longitude', null) );
 		$numResults = floatval( min( $params->fromQuery('numberResults', 10), 100 ) );
+
+		if ( $lat === null || $lng === null )
+			throw new UnprocessableEntityException("The query parameters 'latitude' and 'longitude' must be present and valid.");
 
 		$results = $this->locationService->findNearbyLocations( $lat, $lng, $numResults );
 		return $this->createDefaultViewModel( $this->extractObject( CityHydrator::class, $results ) );
