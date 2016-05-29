80481dca245496dbc7c666e06eade8a1a2b33144
Jannik Portz <hello@jannikportz.de>
2016-05-30 01:31:30 +0200

    added doc for endpoint

--- app/module/Perna/src/Perna/Controller/Weather/WeatherLocationController.php
+++ app/module/Perna/src/Perna/Controller/Weather/WeatherLocationController.php
@@ -7,6 +7,7 @@ use Perna\Controller\AbstractAuthenticatedApiController;
 use Perna\Hydrator\CityHydrator;
 use Perna\Service\AuthenticationService;
 use Perna\Service\WeatherLocationService;
+use Swagger\Annotations as SWG;
 
 class WeatherLocationController extends AbstractAuthenticatedApiController {
 
@@ -19,7 +20,33 @@ class WeatherLocationController extends AbstractAuthenticatedApiController {
 		parent::__construct( $authenticationService );
 		$this->weatherLocationService = $weatherLocationService;
 	}
-	
+
+	/**
+	 * @SWG\Get(
+	 *   path="/weather/locations/{id}",
+	 *   summary="Get Weather Location",
+	 *   description="Retrieves a single weather location item by its id",
+	 *   operationId="getWeatherLocation",
+	 *   tags={"weather"},
+	 *   @SWG\Parameter(
+	 *    in="path",
+	 *    name="id",
+	 *    type="number",
+	 *    format="int32",
+	 *    required=true,
+	 *    description="The id of the weather location to return.",
+	 *    default=123456
+	 *   ),
+	 *   @SWG\Response(
+	 *    response="200",
+	 *    description="Weather location has successfully been retrieved",
+	 *    @SWG\Schema(
+	 *      @SWG\Property(property="success", type="boolean", default="true"),
+	 *      @SWG\Property(property="data", ref="City")
+	 *    )
+	 *   )
+	 * )
+	 */
 	public function get ( array $params ) {
 		$this->assertAccessToken();
 		$this->authenticationService->findAuthenticatedUser( $this->accessToken );
