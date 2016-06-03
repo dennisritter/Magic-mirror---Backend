34bdc53131f337c59d2c90fa343038ae73df9f31
Jannik Portz <hello@jannikportz.de>
2016-06-03 12:30:03 +0200

    added some helper methods to AbstractControllerTestCase

--- app/module/Perna/test/Controller/AbstractControllerTestCase.php
+++ app/module/Perna/test/Controller/AbstractControllerTestCase.php
@@ -11,4 +11,39 @@ class AbstractControllerTestCase extends AbstractHttpControllerTestCase {
 		parent::setUp();
 	}
 
+	protected function getJSONResponse () {
+		$response = $this->getResponse();
+		$data = json_decode( $response->getContent(), true );
+
+		if ( $data === false )
+			throw new \AssertionError("The Response data is not valid JSON.");
+
+		return $data;
+	}
+
+	protected function getSuccessResponseData () {
+		$data = $this->getJSONResponse();
+		$this->assertArrayHasKey('success', $data);
+		$this->assertTrue( $data['success'] );
+		$this->assertArrayHasKey('data', $data);
+		return $data['data'];
+	}
+
+	protected function getErrorResponseContent ( int $status ) {
+		$data = $this->getJSONResponse();
+		$this->assertArrayHasKey('status_code', $data);
+		$this->assertEquals( $status, $data['status_code'] );
+		$this->assertArrayHasKey('message', $data);
+		return $data;
+	}
+
+	protected function assertControllerIs ( $controllerClass ) {
+		$this->assertControllerName( $controllerClass );
+
+		if ( !class_exists( $controllerClass ) )
+			throw new \AssertionError("The controller class {$controllerClass} does not exist.");
+
+		$reflect = new \ReflectionClass( $controllerClass );
+		$this->assertControllerClass( $reflect->getShortName() );
+	}
 }
--- app/module/Perna/test/Controller/RegisterControllerTest.php
+++ app/module/Perna/test/Controller/RegisterControllerTest.php
@@ -6,8 +6,33 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\DocumentRepository;
 use Perna\Controller\RegisterController;
 use Perna\Document\User;
+use ZfrRest\Http\Exception\Client\UnprocessableEntityException;
 
 class RegisterControllerTest extends AbstractControllerTestCase {
+
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $documentManager;
+
+	/** @var \PHPUnit_Framework_MockObject_MockObject */
+	protected $documentRepository;
+
+	public function setUp () {
+		parent::setUp();
+
+		$repoMock = $this->getMockBuilder( DocumentRepository::class )->disableOriginalConstructor()->getMock();
+
+		$dmMock = $this->getMockBuilder( DocumentManager::class )->disableOriginalConstructor()->getMock();
+		$dmMock->expects( $this->once() )->method('getRepository')
+		       ->with( $this->equalTo( User::class ) )
+		       ->willReturn( $repoMock );
+
+		$this->documentManager = $dmMock;
+		$this->documentRepository = $repoMock;
+
+		$sm = $this->getApplicationServiceLocator();
+		$sm->setAllowOverride( true );
+		$sm->setService( DocumentManager::class, $dmMock );
+	}
 	
 	public function testRegistrationSuccess () {
 		$requestData = [
@@ -17,40 +42,53 @@ class RegisterControllerTest extends AbstractControllerTestCase {
 			'password' => 'auvgqovgrovo'
 		];
 
-		$repoMock = $this->getMockBuilder( DocumentRepository::class )->disableOriginalConstructor()->getMock();
-		$repoMock->expects( $this->once() )->method('findBy')
-			->with( $this->equalTo( ['email' => $requestData['email'] ] ) )
-			->willReturn( [] );
+		$this->documentRepository
+			->expects( $this->once() )
+			->method('findBy')
+			->with( $this->equalTo( ['email' => $requestData['email']] ) );
 
-		$dmMock = $this->getMockBuilder( DocumentManager::class )->disableOriginalConstructor()->getMock();
-		$dmMock->expects( $this->once() )->method('getRepository')
-			->with( $this->equalTo( User::class ) )
-			->willReturn( $repoMock );
+		$this->documentManager
+			->expects( $this->once() )
+			->method('flush');
 
-		$dmMock->expects( $this->once() )->method('persist')
+		$this->documentManager
+			->expects( $this->once() )
+			->method('persist')
 			->with( $this->isInstanceOf( User::class ) );
 
-		$dmMock->expects( $this->once() )->method('flush');
-		$sm = $this->getApplicationServiceLocator();
-		$sm->setAllowOverride( true );
-		$sm->setService( DocumentManager::class, $dmMock );
-
 		$this->dispatch( '/v1/register', 'POST', $requestData );
-		$this->assertControllerName( RegisterController::class );
+		$this->assertControllerIs( RegisterController::class );
 
 		$this->assertResponseStatusCode( 201 );
-		$response = $this->getResponse();
-		$content = json_decode( trim( $response->getContent() ), true );
+		$data = $this->getSuccessResponseData();
 
-		$this->assertTrue( is_array( $content ) );
-		$this->assertArrayHasKey( 'success', $content );
-		$this->assertTrue( $content['success'] );
-		$this->assertArrayHasKey('data', $content);
-
-		$data = $content['data'];
 		unset( $requestData['password'] );
 		$this->assertArraySubset( $requestData, $data );
 		$this->assertArrayHasKey( 'lastLogin', $data );
 		$this->assertNull( $data['lastLogin'] );
+	}
+
+	public function testUserAlreadyExists () {
+		$requestData = [
+			'email' => 'meine@emailadresse.de',
+			'firstName' => 'Jannik',
+			'lastName' => 'Portz',
+			'password' => 'dgslfmmbth'
+		];
+
+		$this->documentRepository
+			->expects( $this->once() )
+			->method('findBy')
+			->with( $this->equalTo(['email' => $requestData['email']]) )
+			->willReturn( [ new User() ] );
+
+		$this->documentManager->expects( $this->never() )->method('persist');
+		$this->documentManager->expects( $this->never() )->method('flush');
+
+		$this->dispatch( '/v1/register', 'POST', $requestData );
+		$this->assertControllerIs( RegisterController::class );
+		
+		$this->assertResponseStatusCode( 422 );
+		$this->getErrorResponseContent( 422 );
 	}
 }
