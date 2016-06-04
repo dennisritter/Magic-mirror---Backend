a472d6556494b40a829da77dc2128acec1608ef0
Jannik Portz <hello@jannikportz.de>
2016-06-04 17:44:54 +0200

    added swagger doc for endpoint /weather/locations/search

--- app/module/Perna/src/Perna/Controller/Weather/WeatherLocationSearchController.php
+++ app/module/Perna/src/Perna/Controller/Weather/WeatherLocationSearchController.php
@@ -7,6 +7,7 @@ use Perna\Hydrator\CityHydrator;
 use Perna\Service\AuthenticationService;
 use Perna\Service\Weather\GeoNamesAccessService;
 use ZfrRest\Http\Exception\Client\UnprocessableEntityException;
+use Swagger\Annotations as SWG;
 
 class WeatherLocationSearchController extends AbstractAuthenticatedApiController {
 
@@ -20,6 +21,34 @@ class WeatherLocationSearchController extends AbstractAuthenticatedApiController
 		$this->geoNamesService = $geoNamesService;
 	}
 
+	/**
+	 * @SWG\Get(
+	 *   path="/weather/locations/search",
+	 *   summary="Weather Location Search",
+	 *   description="Searches for the provided query using GeoNames.org API. It is not recommended to use this endpoint for auto-completion but rather for searching with complete city names.",
+	 *   operationId="weatherLocationsSearch",
+	 *   tags={"weather"},
+	 *   @SWG\Parameter(
+	 *    in="query",
+	 *    name="query",
+	 *    type="string",
+	 *    required=true,
+	 *    description="The search query for the weather locations (required)",
+	 *    default="Berlin"
+	 *   ),
+	 *   @SWG\Parameter(ref="#/parameters/accessToken"),
+	 *   @SWG\Response(
+	 *    response="200",
+	 *    description="Locations matching the provided search string.",
+	 *    @SWG\Schema(
+	 *      @SWG\Property(property="success", type="boolean", default=true),
+	 *      @SWG\Property(property="data", type="array", description="The search results", @SWG\Items(ref="City", maxItems=20))
+	 *    ),
+	 *   ),
+	 *   @SWG\Response(response="403", ref="#/responses/403"),
+	 *   @SWG\Response(response="422", ref="#/responses/422")
+	 * )
+	 */
 	public function get () {
 		$this->assertAccessToken();
 		// Check if any user is actually authenticated
