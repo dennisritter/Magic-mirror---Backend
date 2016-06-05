2aa063d6ab33e41a7a29f7248a590fa7b252d1ad
Jannik Portz <hello@jannikportz.de>
2016-06-05 14:41:20 +0200

    documented class AbstractControllerTestCase

--- app/module/Perna/test/Controller/AbstractControllerTestCase.php
+++ app/module/Perna/test/Controller/AbstractControllerTestCase.php
@@ -6,12 +6,18 @@ use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
 class AbstractControllerTestCase extends AbstractHttpControllerTestCase {
 
+	/** @inheritdoc */
 	public function setUp () {
 		$this->setApplicationConfig( include __DIR__ . '/../../../../config/application.config.php' );
 		$this->traceError = false;
 		parent::setUp();
 	}
 
+	/**
+	 * Parses the JSON Response-Content and returns it
+	 * @return    mixed     The JSON data in the response
+	 * @throws    \AssertionError   If the response data is no valid JSON
+	 */
 	protected function getJSONResponse () {
 		$response = $this->getResponse();
 		$data = json_decode( $response->getContent(), true );
@@ -22,6 +28,10 @@ class AbstractControllerTestCase extends AbstractHttpControllerTestCase {
 		return $data;
 	}
 
+	/**
+	 * Returns the parsed Response data and makes assertions concerning success responses
+	 * @return    mixed     The response data under the 'data' key
+	 */
 	protected function getSuccessResponseData () {
 		$data = $this->getJSONResponse();
 		$this->assertArrayHasKey('success', $data);
@@ -30,6 +40,11 @@ class AbstractControllerTestCase extends AbstractHttpControllerTestCase {
 		return $data['data'];
 	}
 
+	/**
+	 * Returns the parsed Response data and males assertions concerning error responses
+	 * @param     int       $status   The expected status code
+	 * @return    mixed               The whole response content
+	 */
 	protected function getErrorResponseContent ( int $status ) {
 		$data = $this->getJSONResponse();
 		$this->assertArrayHasKey('status_code', $data);
@@ -38,6 +53,11 @@ class AbstractControllerTestCase extends AbstractHttpControllerTestCase {
 		return $data;
 	}
 
+	/**
+	 * Makes controller assertions
+	 * @param     string    $controllerClass  The full controller class
+	 * @throws    \AssertionError             If the expected and actual controller do not match
+	 */
 	protected function assertControllerIs ( $controllerClass ) {
 		$this->assertControllerName( $controllerClass );
 
