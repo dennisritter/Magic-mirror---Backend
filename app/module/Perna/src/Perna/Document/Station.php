87d6ff0233153935e453b76b5bed867e7f71acef
Jannik Portz <hello@jannikportz.de>
2016-06-12 18:42:43 +0200

    added odm annotations for document Station

--- app/module/Perna/src/Perna/Document/Station.php
+++ app/module/Perna/src/Perna/Document/Station.php
@@ -2,34 +2,62 @@
 
 namespace Perna\Document;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * Document representing a train station
+ *
+ * @ODM\Document(
+ *   db="perna",
+ *   collection="stations"
+ * )
+ *
+ * @author      Jannik Portz
+ * @package     Perna\Document
+ */
 class Station {
 
 	/**
 	 * The VBB Id
+	 *
+	 * @ODM\Field(type="string")
+	 *
 	 * @var       string
 	 */
 	protected $id;
 
 	/**
 	 * The external ID of the station
+	 *
+	 * @ODM\Field(type="string")
+	 *
 	 * @var       string
 	 */
 	protected $extId;
 
 	/**
 	 * The name of the station
+	 *
+	 * @ODM\Field(type="string")
+	 *
 	 * @var       string
 	 */
 	protected $name;
 
 	/**
 	 * The Geo-Location of the Station as [lat, lng] array
+	 *
+	 * @ODM\Field(type="collection")
+	 *
 	 * @var       array
 	 */
 	protected $location;
 
 	/**
-	 * Array of products that are available on this station
+	 * Array of product-identifiers that are available on this station
+	 *
+	 * @ODM\Field(type="collection")
+	 *
 	 * @var       array
 	 */
 	protected $products;
